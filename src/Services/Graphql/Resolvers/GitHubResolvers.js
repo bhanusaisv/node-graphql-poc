e341e1578955bbb933d8bc213b3bcf7f17afe4b3
BhanuSaiKumarSV <bhanusai.sv@ggktech.com>
2018-07-03 14:55:14 +0530

    feat(repos-functionality):Added get public repos functionality for single or all users

--- src/Services/Graphql/Resolvers/GitHubResolvers.js
+++ src/Services/Graphql/Resolvers/GitHubResolvers.js
@@ -2,23 +2,46 @@ var rp = require('request-promise');
 var { GitHubBaseURL } = require('../../../Utils/Constants')
 
 var getUserDetails = (data) => {
-    var { user_name } = data
-    var options = {
-      uri: `${GitHubBaseURL}/users/${user_name}`,
-      headers: {'User-Agent': 'Request-Promise'},
-      qs: {
-        access_token: '8d5ad13f0c054e6ff7bd79d66e2be856daf95a96' // -> uri + '?access_token=xxxxx%20xxxxx'
-      },
-      json: true
-    };
-      
-    return rp(options)
-      .then(function (res) {
-        return res
-      })
-      .catch(function (err) {
-        return err
-      })
+  var { user_name } = data
+  var options = {
+    uri: `${GitHubBaseURL}/users/${user_name}`,
+    headers: {'User-Agent': 'Request-Promise'},
+    // qs: {
+    //   access_token: '8d5ad13f0c054e6ff7bd79d66e2be856daf95a96' // -> uri + '?access_token=xxxxx%20xxxxx'
+    // },
+    json: true
+  };
+    
+  return rp(options)
+    .then(function (res) {
+      return res
+    })
+    .catch(function (err) {
+      return err
+    })
 }
 
-module.exports = { getUserDetails }
+var getPublicRepos = (data) => {
+  var { user_name } = data
+  console.log(data,'data')
+  var options = {
+    uri: (`${user_name}` == 'all') ? `${GitHubBaseURL}/repositories` : `${GitHubBaseURL}/users/${user_name}/repos`,
+    headers: {'User-Agent': 'Request-Promise'},
+    // qs: {
+    //   access_token: '8d5ad13f0c054e6ff7bd79d66e2be856daf95a96' // -> uri + '?access_token=xxxxx%20xxxxx'
+    // },
+    json: true
+  }
+  console.log('options',options)
+  
+
+  return rp(options)
+  .then(function (res) {
+    return res
+  })
+  .catch(function (err) {
+    return err
+  })
+}
+
+module.exports = { getUserDetails, getPublicRepos }
--- src/Services/Graphql/Resolvers/resolvers.js
+++ src/Services/Graphql/Resolvers/resolvers.js
@@ -1,8 +1,8 @@
-var { getUserDetails } = require('./GitHubResolvers')
+var {  getPublicRepos, getUserDetails } = require('./GitHubResolvers')
 
 var resolvers = {
-    user: (user_mail) => getUserDetails(user_mail),
-    repositories: () => 'Here Are Your Repositories'
+    repositories: (userNameorAllUserArgs) => getPublicRepos(userNameorAllUserArgs),    
+    user: (userNameArgs) => getUserDetails(userNameArgs)
 };
 
 module.exports = { resolvers };
